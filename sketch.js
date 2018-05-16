ebe5a35aa6de6f36b3712401a0528763b70659b8
Suhail Ahamed <suhailahamed@Suhails-MacBook-Pro.local>
2018-05-16 16:16:23 +0400

    get bottom line

--- sketch.js
+++ sketch.js
@@ -1,50 +1,57 @@
-//Suhail's First p5 Project
+/*
 
-var myWidth = 600, myHeight = 400;
-let c1 = [];
+Author: Suhail Ahamed
+Project: CodingFlames challenge, https://www.youtube.com/watch?v=X0kjv0MozuY
+Date: 16/05/2018
 
-class circle{
-	constructor(_x,_y,_r){
-		this.radius=_r;
-		this.x=_x,this.y=_y;
-		this.speedX=random(-6,6),this.speedY=random(-8,8);
-	}
-	draw(){
-		ellipse(this.x,this.y,this.radius);
-	}
-	updateLoc(){
-		this.x+=this.speedX;
-		this.y+=this.speedY;
-		if(this.x>myWidth-this.radius/2 || this.x<this.radius/2){
-			this.speedX*=-1;
+*/
 
-		}
-		if(this.y>myHeight-this.radius/2 || this.y<this.radius/2){
-			this.speedY*=-1;
-		}
-	}
-};
-
-function mousePressed(){
-
-	let c = new circle(mouseX,mouseY,random(15,50));
-	c1.push(c);
-
-}
+var buffer1 = [], buffer2 = [];
+var rows, cols;
 
 
 function setup() {
-	//createCanvas(windowWidth, windowHeight);
-	createCanvas(myWidth, myHeight);
+	createCanvas(windowWidth, windowHeight);
+	pixelDensity(1);
+
+	rows = windowWidth; cols = windowHeight;
+	for(var i=0;i<rows-1;i++){
+		buffer1[i] = [];
+		buffer2[i] = [];
+			for(var j=0;j<cols-1;j++){
+					buffer1[i][j] = 0.0;
+					buffer2[i][j] = 0.0;
+			}
+	}
+}
 
 
+function bottomLine(noOfRows){
+
+	for(var i=0;i<rows-1;i++){
+		buffer1[i] = [];
+		buffer2[i] = [];
+			for(var j=rows-1;j>rows-noOfRows-1;j--){
+					buffer1[i][j] = 255.0;
+					buffer2[i][j] = 255.0;
+			}
+	}
 }
 
 function draw() {
-
-	background(24,49,68);
-	for(let i=0;i<(c1.length);i++){
-		c1[i].draw();
-		c1[i].updateLoc();
+	bottomLine(100);
+	// background(24,49,68);
+	loadPixels();
+	for(var i=0;i<rows-1;i++){
+			for(var j=0;j<cols-1;j++){
+				index = (i+j*rows)*4;
+				pixels[index+0] = buffer1[i][j];
+				pixels[index+1] = buffer1[i][j];
+				pixels[index+2] = buffer1[i][j];
+				pixels[index+3] = 255;
+			}
 	}
+
+	updatePixels();
+
 }
